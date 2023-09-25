7882c2bd1e9d9e316b659bb0575e306d73e9ba10
GabrielEduardoSilva <gaebds@gmail.com>
2023-09-25 11:19:58 -0300

    Definindo a estrutura da UITableView

---
+++ ToDoListUIKit/ToDoListUIKit/ToDo.swift
@@ -0,0 +1,13 @@
+//
+//  ToDo.swift
+//  ToDoListUIKit
+//
+//  Created by Gabriel Eduardo on 25/09/23.
+//
+
+import Foundation
+
+struct Todo {
+    let title: String
+    let isComplete: Bool = false
+}
--- ToDoListUIKit/ToDoListUIKit/ViewController.swift
+++ ToDoListUIKit/ToDoListUIKit/ViewController.swift
@@ -8,12 +8,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let todos = [
+    Todo(title: "Make vanilla pudding."),
+    Todo(title: "Put pudding in a mayo jarl"),
+    Todo(title: "Eat it in a public place."),
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+}
+
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 100
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        
+        cell.textLabel!.text = "Teste"
+        
+        return cell
+    }
 }
 
